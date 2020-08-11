bc1b971c161bd9d323072cc0180a13cdf29c3cb0
mori-hisayuki <h.mori0206@gmail.com>
2020-08-11 12:57:30 +0900

    Teemaの適用

--- packages/www/gatsby-config.js
+++ packages/www/gatsby-config.js
@@ -1,16 +1,1 @@
-module.exports = {
-  plugins: [
-    {
-      resolve: `gatsby-plugin-typescript`,
-      options: {
-        isTSX: true,
-        jsxPragma: `jsx`,
-        allExtensions: true,
-      },
-      resolve: "gatsby-plugin-graphql-codegen",
-      options: {
-        fileName: `types/graphql-types.d.ts`,
-      },
-    },
-  ],
-}
+module.exports = {}
