6e31d0e4c65679923e9e2fff42f9a7434e18f0f6
mori-hisayuki <h.mori0206@gmail.com>
2020-08-11 17:35:43 +0900

    IdentityWidgetの実装

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,6 +1,28 @@
 import React from 'react'
-export default props => (
-  <div>
-    <h1>Our site</h1>
-  </div>
-)
+import { Container, Heading, Button, Flex} from 'theme-ui'
+import netlifyIdentity from 'netlify-identity-widget'
+import { useEffect } from 'react'
+
+
+export default props => {
+  useEffect(() => {
+    console.log('netlify-identity-widget init')
+    netlifyIdentity.init({})
+  })
+
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: "column", padding: 3}}>
+        <Heading as="h1">Get Stuff Done</Heading>
+        <Button
+          sx={{ marginTop:2 }}
+          onClick={() =>{
+            netlifyIdentity.open()
+          }}
+        >
+          Log In
+        </Button>
+      </Flex>
+    </Container>
+  )
+}
