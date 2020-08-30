bf4bec6a3e1456cf550a6c267d06cd2d426a7177
mori-hisayuki <h.mori0206@gmail.com>
2020-08-30 21:40:48 +0900

    functionの更新

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -21,8 +21,12 @@ const todoIndex = 0
 
 const resolvers = {
   Query: {
-    todos: () => {
-      return Object.values(todos);
+    todos: (parent, args, {user}) => {
+      if (!user) {
+        return []
+      } else {
+        return Object.values(todos)
+      }
     },
   },
   Mutation: {
@@ -41,6 +45,13 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: ({ context }) => {
+    if (context.clientContext.user) {
+      return { user: context.clientContext.user.sub }
+    } else {
+      return {}
+    }
+  },
   playground: true,
   introspection: true
 })
--- packages/www/gatsby-browser.js
+++ packages/www/gatsby-browser.js
@@ -1,12 +1,29 @@
 const React = require('react')
 const {ApolloProvider, ApolloClient, InMemoryCache, HttpLink} = require('@apollo/client')
+const { setContext } = require('apollo-link-context')
+const netlifyIdentity = require('netlify-identity-widget')
+
 const wrapRootElement = require('./wrap-root-element')
+
+const authLink = setContext((_, {headers}) => {
+  const user = netlifyIdentity.currentUser()
+  const token = user.token.access_token
+
+  return {
+    headers: {
+      ...headers,
+      Authorization: token  ? `Bearer ${token}` : ""
+    }
+  }
+})
+
+const httpLink = new HttpLink({
+    url: 'https://jamstack-todo-netlify-faunadb.netlify.app/.netlify/functions/graphql'
+})
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    url: 'https://jamstack-todo-netlify-faunadb.netlify.app/.netlify/functions/graphql'
-  })
+  link: authLink.concat(httpLink)
 })
 
 exports.wrapRootElement = ({ element }) => (
