5bfb172e8fadbde0e594a0eae5113593f8780fc3
mori-hisayuki <h.mori0206@gmail.com>
2020-08-30 17:06:32 +0900

    ToDoの取得をGraphQLで行う

--- packages/www/gatsby-browser.js
+++ packages/www/gatsby-browser.js
@@ -1,4 +1,16 @@
 const React = require('react')
+const {ApolloProvider, ApolloClient, InMemoryCache, HttpLink} = require('@apollo/client')
 const wrapRootElement = require('./wrap-root-element')
 
-exports.wrapRootElement = wrapRootElement
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    url: 'https://jamstack-todo-netlify-faunadb.netlify.app/.netlify/functions/graphql'
+  })
+})
+
+exports.wrapRootElement = ({ element }) => (
+  <ApolloProvider client={client}>
+    {wrapRootElement({ element })}
+  </ApolloProvider>
+)
--- packages/www/src/pages/dashbord.js
+++ packages/www/src/pages/dashbord.js
@@ -1,7 +1,34 @@
 import React, { useContext, useState, useRef, useReducer } from 'react'
 import { Router, Link } from "@reach/router"
 import { Container, Flex, Button, NavLink, Label, Input, Checkbox } from 'theme-ui'
+import { gql, useMutation, useQuery } from '@apollo/client'
 import { IdentityContext } from '../../identity-context'
+
+const ADD_TODO = gql`
+  mutation AddTodo($type: String!) {
+    addTodo(type: $text) {
+      id
+    }
+  }
+`
+
+const UPDATE_TODO_DONE = gql`
+  mutation UpdateTodoDone($id: ID!) {
+    updateTodoDone(id: $id) {
+      text
+      done
+    }
+  }
+`
+const GET_TODOS = gql`
+  query GetTodos {
+    todos {
+      id
+      text
+      done
+    }
+  }
+`
 
 const todosReducer = (state, action) => {
   switch(action.type) {
@@ -22,49 +49,58 @@ export default () => {
   const { user, identity: netlifyIdentity } = useContext(IdentityContext)
   const [todos, dispatch] = useReducer(todosReducer, [])
   const inputRef = useRef()
+  const [addTodo] = useMutation(ADD_TODO)
+  const [updateTodoDone] = useMutation(UPDATE_TODO_DONE)
+  const { loading, error, data} = useQuery(GET_TODOS)
+
   return (
     <Container>
-      <Flex as='nav'>
-        <NavLink as={Link} to='/' p={2}>
+      <Flex as="nav">
+        <NavLink as={Link} to="/" p={2}>
           Home
         </NavLink>
-        <NavLink as={Link} to='/app' p={2}>
+        <NavLink as={Link} to="/app" p={2}>
           Dashborad
         </NavLink>
         {user && (
-          <NavLink href='#!' p={2} onClick={() => netlifyIdentity.logout()}>
+          <NavLink href="#!" p={2} onClick={() => netlifyIdentity.logout()}>
             Log out {user.user_metadata.full_name}
           </NavLink>
         )}
       </Flex>
-      <Flex as='form' onSubmit={e=>{
-        e.preventDefault()
-        dispatch({ type: 'addTodo', payload: inputRef.current.value})
-        inputRef.current.value = ''
-      }}>
-        <Label sx={{ display: 'flex'}}>
+      <Flex
+        as="form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          addTodo({ variables: { test: inputRef.current.value } });
+          inputRef.current.value = "";
+        }}
+      >
+        <Label sx={{ display: "flex" }}>
           <span>Add Todo</span>
-          <Input ref={inputRef} sx={{marginLeft: 1}}></Input>
+          <Input ref={inputRef} sx={{ marginLeft: 1 }}></Input>
         </Label>
-        <Button sx={{ marginLeft: 1}}>Submit</Button>
+        <Button sx={{ marginLeft: 1 }}>Submit</Button>
       </Flex>
-      <Flex sx={{ flexDirection: 'column' }}>
-        <ul sx={{ listStyle: 'none'}}>
-          {todos.map((todo, i) => (
-            <Flex as='li'
-              onClick={e =>{
-                dispatch({
-                  type: 'toggleTodoDone',
-                  payload: i
-                })
-              }}
-            >
-              <Checkbox checked={todo.done} />
-              <span>{todo.value}</span>
-            </Flex>
-          ))}
-        </ul>
+      <Flex sx={{ flexDirection: "column" }}>
+        {loading ? <div>loading...</div> : null}
+        {error ? <div>{error.message}</div> : null}
+        {!loading && !error && (
+          <ul sx={{ listStyle: "none" }}>
+            {todos.map(todo => (
+              <Flex
+                as="li"
+                onClick={() => {
+                  updateTodoDone({ variables: {id: todo.id}})
+                }}
+              >
+                <Checkbox checked={todo.done} />
+                <span>{todo.value}</span>
+              </Flex>
+            ))}
+          </ul>
+        )}
       </Flex>
     </Container>
-  )
+  );
 }
