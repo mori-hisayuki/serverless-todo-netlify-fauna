fa1ac6b92587322d564c059392285653ba909954
mori-hisayuki <h.mori0206@gmail.com>
2020-09-02 23:33:15 +0900

    途中まで

--- packages/www/fauna-test/index.js
+++ packages/www/fauna-test/index.js
@@ -1,4 +1,20 @@
 const faunadb = require('faunadb')
 const q = faunadb.query
 
+console.log('emv:' + process.env.FAUNA)
 var client = new faunadb.Client({ sercret: process.env.FAUNA})
+
+async function run() {
+    const results = await client.query(
+        q.Create(q.Collection('todos'), {
+            data: {
+                text: 'whatever',
+                done: false,
+                owner: 'user-test'
+            }
+        })
+    )
+    console.log(results)
+}
+
+run()
