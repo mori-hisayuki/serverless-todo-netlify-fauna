e2634bc3d3e0801251c473cf462dec68339b8061
mori-hisayuki <h.mori0206@gmail.com>
2020-09-03 21:42:04 +0900

    dashboard修正

--- packages/www/src/pages/app.js
+++ packages/www/src/pages/app.js
@@ -2,46 +2,12 @@ import React, { useContext } from 'react'
 import { Router, Link } from "@reach/router"
 import { Container, Flex, Heading, Button, NavLink } from 'theme-ui'
 import { IdentityContext } from '../../identity-context'
-
-let Dash = () => {
-  const { user, identity: netlifyIdentity } = useContext(IdentityContext)
-  return (
-    <Container>
-      <Flex as='nav'>
-        <NavLink as={Link} to='/' p={2}>
-          Home
-        </NavLink>
-        <NavLink as={Link} to='/app' p={2}>
-          Dashborad
-        </NavLink>
-        {user && (
-          <NavLink href='#!' p={2} onClick={() => netlifyIdentity.logout()}>
-            Log out {user.user_metadata.full_name}
-          </NavLink>
-        )}
-      </Flex>
-      <div>Dash hasUser: {user && user.user_metadata.full_name}</div>
-    </Container>
-  )
-}
+import Dash from "../components/dashboard"
 
 let DashLoggedOut = props => {
   const { user, identity: netlifyIdentity } = useContext(IdentityContext)
   return (
-    <Flex>
-      <Flex as='nav'>
-        <NavLink as={Link} to='/' p={2}>
-          Home
-        </NavLink>
-        <NavLink as={Link} to='/app' p={2}>
-          Dashborad
-        </NavLink>
-        {user && (
-          <NavLink href='#!' p={2}>
-            {user.user_metadata.full_name}
-          </NavLink>
-        )}
-      </Flex>
+    <Container>
       <Flex sx={{ flexDirection: "column", padding: 3}}>
         <Heading as="h1">Get Stuff Done</Heading>
         <Button
@@ -53,7 +19,7 @@ let DashLoggedOut = props => {
           Log In
         </Button>
       </Flex>
-    </Flex>
+    </Container>
   )
 }
 
