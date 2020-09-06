9595f5dba0070783da0ef2334714e7e8ab39dabe
mori-hisayuki <h.mori0206@gmail.com>
2020-09-06 10:45:46 +0900

    functionの更新

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -1,4 +1,9 @@
 const {ApolloServer, gql} = require('apollo-server-lambda')
+const faunadb = require('faunadb')
+const q = faunadb.query
+
+const client = new faunadb.Client({secret: process.env.FAUNA})
+
 
 const typeDefs = gql`
   type Query {
@@ -16,28 +21,52 @@ const typeDefs = gql`
   }
 `;
 
-const todos = {}
-let todoIndex = 0
-
 const resolvers = {
   Query: {
     todos: (parent, args, {user}) => {
       if (!user) {
         return []
       } else {
-        return Object.values(todos)
+        const results = await client.query(
+            q.Paginate(q.Match(q.Index('todos_by_user'), 'user-test'))
+        )
+        return results.data.map(([ref, text, done]) => {
+          id: ref.id,
+          text,
+          done
+        })
       }
     },
   },
   Mutation: {
-    addTodo: (_, {text}) => {
-      todoIndex++
-      const id = `key-${todoIndex}`
-      todos[id] = {id, text, done: false}
+    addTodo: async (_, {text}, {user}) => {
+      if (!user) throw new Error('Must be authenticated to insert todos')
+      const results = await client.query(
+        q.Create(q.Collection("todos"), {
+          data: {
+            text,
+            done: false,
+            owner: user
+          }
+        })
+      )
+      return {
+        ...results.data,
+        id: results.ref.id
+      }
     },
     updateTodoDone: (_, {id}) => {
-      todos[id].done = true
-      return todos[id]
+      const results = await client.query(
+        q.Update(q.Ref(q.Collection("todos"), id), {
+          data: {
+            done: false
+          }
+        })
+      )
+      return {
+        ...results.data,
+        id: results.ref.id
+      }
     }
   }
 };
